f46b801972703cd8ff19c3665ab9a1b064720ca7
MatteoGaloni <mtgaloni@gmail.com>
2023-07-17 16:20:59 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppMain from './components/Appmain.vue'
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    AppMain
+  }
+}
+</script>
+
+<template>
+  <h1>CIAO SONO VUE!!</h1>
+  <AppMain />
+</template>
+
+<style scoped></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from 'axios';
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            apiUrl: "http://127.0.0.1:8000/api/",
+            projectsApi: "projects",
+            loading: false,
+            projects: []
+        }
+
+    },
+    methods() {
+
+
+    },
+
+    mounted() {
+        axios.get(this.apiUrl + 'projects').then((response) => {
+            this.projects = response.data.results.data;
+            console.log(response.data);
+        });
+    }
+
+}
+</script>
+
+<template>
+    <h2>Ciao sono componente come stai? </h2>
+    <div v-for="project in projects">
+        <h4>{{ project.name }}</h4>
+        <h4>{{ project.type.name }}</h4>
+        <h4> {{ project.description }}</h4>
+
+        <a @click="getNextPage()">Next page</a>
+    </div>
+</template>
