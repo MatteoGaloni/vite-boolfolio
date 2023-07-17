167ba9dfc5a32572bc77d65e0a4b0a23f394edc6
MatteoGaloni <mtgaloni@gmail.com>
2023-07-17 18:57:51 +0200

    add data in ProjectCard

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -10,17 +10,20 @@ export default {
 </script>
 
 <template>
-    <div class="card">
-        <h1 class="text-center m-3">{{ project.title }}</h1>
-        <div>
-            <strong>Type: </strong>
-            <span>{{ project.type ? project.type.name : "Niente da visualizzare" }}</span>
-        </div>
-        <div>
-            <p>{{ project.description }}</p>
-        </div>
-        <div>
-            <p>{{ project.technology }}</p>
+    <div class="card col-4" style=" width: 18rem;">
+        <img class="card-img-top" :src="project.img" :alt="project.type.name">
+        <div class="card-body">
+            <h1 class="text-center m-3">{{ project.title }}</h1>
+            <div>
+                <strong>Type: </strong>
+                <span>{{ project.type ? project.type.name : "Niente da visualizzare" }}</span>
+            </div>
+            <div>
+                <p>{{ project.description }}</p>
+            </div>
+            <div>
+                <p>{{ project.technology }}</p>
+            </div>
         </div>
     </div>
 </template>
