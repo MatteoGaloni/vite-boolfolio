24b9c1c9efd631f11853fbd2c8d0560da1a5c1cd
MatteoGaloni <mtgaloni@gmail.com>
2023-07-19 12:09:29 +0200

    feat(appheader/footer): add components

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,7 +7,24 @@ export default {
 </script>
 
 <template>
-    <h1 class="text-center m-3">SONO APPHEADER</h1>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/allprojects">Projects</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <style  scoped></style>
